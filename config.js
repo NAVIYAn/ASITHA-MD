8e56ea3a70f35f49d57dc7f00c388a00e385f406
NAVIYA 🖤🎊 <nimsaranavindu927@gmail.com>
2024-10-03 11:02:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "",//enter mongo db url
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=mJEQWKBJ#q0RU1RU3DhiofeaBm7jBOMFbo_RXIjQRh82UfbmRX0c",
+MONGODB: process.env.MONGODB || "mongodb://mongo:ReMsMhOSBwDQqIwIKpXbHRanJcJiYNmZ@autorack.proxy.rlwy.net:43688",
 };
